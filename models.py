6524ece5df48a5f30cfe88615a9a4f3dd43e4963
sandeepsinhbarot <104805757+sandeepsinhbarot@users.noreply.github.com>
2025-03-12 21:14:11 -0400

    Increase password field length in User model

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     role = db.Column(db.String(20), nullable=False)
     transactions = db.relationship('Transaction', backref='user', lazy=True)
 
